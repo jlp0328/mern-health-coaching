d41f453edfb8241e54bffd3dd82bcf06854e024b
Pittman, Jamie <jamie.pittman@fmr.com>
2019-12-13 15:50:48 -0500

    add datepicker

--- src/components/WeightLog/Weight.js
+++ src/components/WeightLog/Weight.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function Weight() {
 	return <h1>Hello</h1>;
