94a5d486d80c152f0cb6795dbddc532c85c53c7d
Pittman, Jamie <jamie.pittman@fmr.com>
2019-12-18 13:41:44 -0500

    Fixed macros submit disable + pass client info to Weight route

--- src/App.js
+++ src/App.js
@@ -1,107 +1,107 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
-import ClientMain from "./components/ClientMain/ClientMain";
-import ClientNav from "./components/ClientNav/ClientNav";
-import CheckIn from "./components/CheckIn/CheckIn";
-import Macros from "./components/MacrosLog/Macros";
-import Measurements from "./components/Measurements/Measurements";
-import Profile from "./components/ClientProfile/Profile";
-import Weight from "./components/WeightLog/Weight";
+import ClientMain from './components/ClientMain/ClientMain';
+import ClientNav from './components/ClientNav/ClientNav';
+import CheckIn from './components/CheckIn/CheckIn';
+import Macros from './components/MacrosLog/Macros';
+import Measurements from './components/Measurements/Measurements';
+import Profile from './components/ClientProfile/Profile';
+import Weight from './components/WeightLog/Weight';
 
-import "./App.css";
+import './App.css';
 
 const drawerWidth = 240;
 
 //Will need to come back and add Redux to manage state when drawer is open to shift main's contents
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  }
+	root: {
+		display: 'flex',
+	},
+	drawerHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		padding: theme.spacing(0, 1),
+		...theme.mixins.toolbar,
+		justifyContent: 'flex-end',
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3),
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		marginLeft: -drawerWidth,
+	},
+	contentShift: {
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+		marginLeft: 0,
+	},
 }));
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      personal: {},
-      goals: {}
-    };
-  }
-  async componentDidMount() {
-    try {
-      const clientInfo = await axios.get(
-        `http://${process.env.REACT_APP_BACKEND_IP}:5000/clients/5de92ab896558099cbcdbdde`
-      );
+		this.state = {
+			personal: {},
+			goals: {},
+		};
+	}
+	async componentDidMount() {
+		try {
+			const clientInfo = await axios.get(
+				`http://${process.env.REACT_APP_BACKEND_IP}:5000/clients/5dee532f975664ad3bd47094`,
+			);
 
-      console.log(clientInfo);
-      const clientGoals = await axios.get(
-        `http://${process.env.REACT_APP_BACKEND_IP}:5000/clients/goals/${clientInfo.data._id}`
-      );
+			console.log(clientInfo);
+			const clientGoals = await axios.get(
+				`http://${process.env.REACT_APP_BACKEND_IP}:5000/clients/goals/${clientInfo.data._id}`,
+			);
 
-      this.setState({
-        personal: clientInfo.data,
-        goals: clientGoals.data
-      });
+			this.setState({
+				personal: clientInfo.data,
+				goals: clientGoals.data,
+			});
 
-      console.log(this.state);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+			console.log(this.state);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <ClientNav />
-          <main className="main-page-container">
-            <Route
-              exact
-              path="/"
-              render={props => <ClientMain client={this.state} />}
-            />
-            <Route path="/profile" component={Profile} />
-            <Route
-              path="/weight"
-              render={props => <Weight client={this.state.clientInfo} />}
-            />
-            <Route path="/macros" component={Macros} />
-            <Route path="/measurements" component={Measurements} />
-            <Route path="/check-in" component={CheckIn} />
-          </main>
-        </Router>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Router>
+					<ClientNav />
+					<main className='main-page-container'>
+						<Route
+							exact
+							path='/'
+							render={props => <ClientMain client={this.state} />}
+						/>
+						<Route path='/profile' component={Profile} />
+						<Route
+							path='/weight'
+							render={props => <Weight client={this.state.personal} />}
+						/>
+						<Route path='/macros' component={Macros} />
+						<Route path='/measurements' component={Measurements} />
+						<Route path='/check-in' component={CheckIn} />
+					</main>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/ClientMain/ClientMainMacros.js
+++ src/components/ClientMain/ClientMainMacros.js
@@ -1,145 +1,146 @@
-import React, { Component } from "react";
-import axios from "axios";
-import moment from "moment";
+import React, { Component } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import { isEmpty } from 'lodash';
 
-import DailyInputsFields from "./DailyInputsFields";
+import DailyInputsFields from './DailyInputsFields';
 
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const inputData = require("./data/macros-inputs.json");
+const inputData = require('./data/macros-inputs.json');
 
 export default class ClientMainMacros extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      client: this.props.client,
-      carbs: "",
-      protein: "",
-      fat: "",
-      fiber: "",
-      submitDisabled: true,
-      needMacros: true,
-      loading: true
-    };
-  }
+		this.state = {
+			client: this.props.client,
+			carbs: '',
+			protein: '',
+			fat: '',
+			fiber: '',
+			submitDisabled: true,
+			needMacros: true,
+			loading: true,
+		};
+	}
 
-  async componentDidUpdate() {
-    const todaysMacros = await axios.get(
-      `http://${process.env.REACT_APP_BACKEND_IP}:5000/macros/${this.props.client._id}`
-    );
+	async componentDidUpdate() {
+		const todaysMacros = await axios.get(
+			`http://${process.env.REACT_APP_BACKEND_IP}:5000/macros/${this.props.client._id}`,
+		);
 
-    if (todaysMacros.data !== null) {
-      const today = moment().format();
-      const latestMacros = moment(todaysMacros.data.date).format(
-        "dddd, MMMM Do YYYY"
-      );
+		if (todaysMacros.data !== null) {
+			const today = moment().format();
+			const latestMacros = moment(todaysMacros.data.date).format(
+				'dddd, MMMM Do YYYY',
+			);
 
-      this.setState({
-        needMacros: today === latestMacros ? false : true,
-        loading: false
-      });
-    } else {
-      this.setState({
-        loading: false
-      });
-    }
-  }
+			this.setState({
+				needMacros: today === latestMacros ? false : true,
+				loading: false,
+			});
+		} else {
+			this.setState({
+				loading: false,
+			});
+		}
+	}
 
-  updateMacros = e => {
-    let update = {};
-    let disable =
-      Object.values(this.state).includes(" ") ||
-      Object.values(this.state).includes(NaN) ||
-      e.target.value === "";
+	validate = async () => {
+		let disable = Object.values(this.state).includes('');
+		this.setState({
+			submitDisabled: disable,
+		});
+	};
 
-    // How to use??
-    // console.log(isNan(e.target.value));
+	updateMacros = async e => {
+		let update = {};
 
-    update["submitDisabled"] = disable;
-    update[e.target.name] = parseInt(e.target.value);
-    this.setState(update);
-    console.log(this.state);
-  };
+		update[e.target.name] = isEmpty(e.target.value)
+			? ''
+			: parseInt(e.target.value);
+		await this.setState(update);
+		await this.validate();
+	};
 
-  submitMacros = async e => {
-    e.preventDefault();
-    let { carbs, protein, fat, fiber } = this.state;
+	submitMacros = async e => {
+		e.preventDefault();
+		let { carbs, protein, fat, fiber } = this.state;
 
-    const todaysMacros = {
-      user: this.props.client._id,
-      carbs,
-      protein,
-      fat,
-      fiber
-    };
+		const todaysMacros = {
+			user: this.props.client._id,
+			carbs,
+			protein,
+			fat,
+			fiber,
+		};
 
-    console.log(todaysMacros);
-    this.setState({
-      loading: true
-    });
+		console.log(todaysMacros);
+		this.setState({
+			loading: true,
+		});
 
-    // await axios.post(
-    // 	`http://${process.env.REACT_APP_BACKEND_IP}:5000/weight/daily-log`,
-    // 	todaysMacros,
-    // );
-  };
+		// await axios.post(
+		// 	`http://${process.env.REACT_APP_BACKEND_IP}:5000/weight/daily-log`,
+		// 	todaysMacros,
+		// );
+	};
 
-  createInputList = () => {
-    return inputData.map((elem, index) => {
-      return (
-        <DailyInputsFields
-          key={index}
-          attributes={elem}
-          change={this.updateMacros}
-        />
-      );
-    });
-  };
+	createInputList = () => {
+		return inputData.map((elem, index) => {
+			return (
+				<DailyInputsFields
+					key={index}
+					attributes={elem}
+					change={this.updateMacros}
+				/>
+			);
+		});
+	};
 
-  macrosFormRender = () => {
-    if (this.state.needMacros) {
-      return (
-        <form noValidate className="client-landing-daily-inputs--macros">
-          {this.createInputList()}
-          <CardActions>
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              disabled={this.state.submitDisabled}
-              onClick={this.submitMacros}
-            >
-              Submit
-            </Button>
-          </CardActions>
-        </form>
-      );
-    } else {
-      return <h3>Thank you for submitting your macros today!</h3>;
-    }
-  };
+	macrosFormRender = () => {
+		if (this.state.needMacros) {
+			return (
+				<form noValidate className='client-landing-daily-inputs--macros'>
+					{this.createInputList()}
+					<CardActions>
+						<Button
+							variant='contained'
+							color='primary'
+							type='submit'
+							disabled={this.state.submitDisabled}
+							onClick={this.submitMacros}>
+							Submit
+						</Button>
+					</CardActions>
+				</form>
+			);
+		} else {
+			return <h3>Thank you for submitting your macros today!</h3>;
+		}
+	};
 
-  render() {
-    let macrosForm;
+	render() {
+		let macrosForm;
 
-    if (this.state.loading) {
-      macrosForm = <CircularProgress />;
-    } else {
-      macrosForm = this.macrosFormRender();
-    }
+		if (this.state.loading) {
+			macrosForm = <CircularProgress />;
+		} else {
+			macrosForm = this.macrosFormRender();
+		}
 
-    return (
-      <Card elevation={3}>
-        <CardContent>
-          <h3>Macros</h3>
-          {macrosForm}
-        </CardContent>
-      </Card>
-    );
-  }
+		return (
+			<Card elevation={3}>
+				<CardContent>
+					<h3>Macros</h3>
+					{macrosForm}
+				</CardContent>
+			</Card>
+		);
+	}
 }
